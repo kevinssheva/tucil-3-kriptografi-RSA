ad7e559c252af8e7fd90692dc58287269df77586
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-04-21 23:51:04 +0700

    feat: generating key

--- src/crypto/rsa.js
+++ src/crypto/rsa.js
@@ -1,82 +1,126 @@
 import random from "crypto-random-prime";
 
-class RSA {
-  constructor(bits) {
-    this.bits = bits;
-    const keys = this.generateKeys();
-    this.publicKey = keys.publicKey;
-    this.privateKey = keys.privateKey;
-  }
+// class RSA {
+//   constructor(bits) {
+//     this.bits = bits;
+//     const keys = this.generateKeys();
+//     this.publicKey = keys.publicKey;
+//     this.privateKey = keys.privateKey;
+//   }
 
-  // Find the greatest common divisor of two numbers
-  gcd(a, b) {
-    if (b === BigInt(0)) return a;
-    return this.gcd(b, a % b);
-  }
+//   // Find the greatest common divisor of two numbers
+//   gcd(a, b) {
+//     if (b === BigInt(0)) return a;
+//     return this.gcd(b, a % b);
+//   }
 
-  // Extended Euclidean Algorithm
-  extendedEuclid(a, b) {
-    for (let i = 1; i < b; i++) {
-      if ((BigInt(1) + BigInt(i) * b) % a === BigInt(0)) {
-        console.log(i);
-        return (BigInt(1) + BigInt(i) * b) / a;
-      }
-    }
-    return BigInt(0);
-  }
+//   // Extended Euclidean Algorithm
+//   extendedEuclid(a, b) {
+//     for (let i = 1; i < b; i++) {
+//       if ((BigInt(1) + BigInt(i) * b) % a === BigInt(0)) {
+//         console.log(i);
+//         return (BigInt(1) + BigInt(i) * b) / a;
+//       }
+//     }
+//     return BigInt(0);
+//   }
 
-  // Generate public and private keys
-  generateKeys() {
-    const p = BigInt(random(this.bits));
-    const q = BigInt(random(this.bits));
-    console.log(p, q);
-    const n = p * q;
-    const phi = (p - BigInt(1)) * (q - BigInt(1));
-    let e = BigInt(17);
-    do {
-      e += BigInt(1);
-    } while (this.gcd(e, phi) !== BigInt(1));
-    const d = this.extendedEuclid(e, phi);
-    console.log(d);
-    return {
-      publicKey: [e, n],
-      privateKey: d,
-    };
-  }
+//   // Generate public and private keys
+//   generateKeys() {
+//     const p = BigInt(random(this.bits));
+//     const q = BigInt(random(this.bits));
+//     console.log(p, q);
+//     const n = p * q;
+//     const phi = (p - BigInt(1)) * (q - BigInt(1));
+//     let e = BigInt(17);
+//     do {
+//       e += BigInt(1);
+//     } while (this.gcd(e, phi) !== BigInt(1));
+//     const d = this.extendedEuclid(e, phi);
+//     console.log(d);
+//     return {
+//       publicKey: [e, n],
+//       privateKey: d,
+//     };
+//   }
 
-  // Encrypt message using public key
-  encrypt(message) {
-    const [e, n] = this.publicKey;
-    return BigInt(message) ** e % n;
-  }
+//   // Encrypt message using public key
+//   encrypt(message) {
+//     const [e, n] = this.publicKey;
+//     return BigInt(message) ** e % n;
+//   }
 
-  // Decrypt message using private key
-  decrypt(encryptedMessage) {
-    const [d, n] = [this.privateKey, this.publicKey[1]];
-    let result = BigInt(1);
-    let base = encryptedMessage;
-    let exp = d;
+//   // Decrypt message using private key
+//   decrypt(encryptedMessage) {
+//     const [d, n] = [this.privateKey, this.publicKey[1]];
+//     let result = BigInt(1);
+//     let base = encryptedMessage;
+//     let exp = d;
 
-    while (exp > BigInt(0)) {
-      if (exp % BigInt(2) === BigInt(1)) {
-        result = (result * base) % n;
-      }
-      base = (base * base) % n;
-      exp = exp / BigInt(2);
-    }
+//     while (exp > BigInt(0)) {
+//       if (exp % BigInt(2) === BigInt(1)) {
+//         result = (result * base) % n;
+//       }
+//       base = (base * base) % n;
+//       exp = exp / BigInt(2);
+//     }
 
-    return result;
-  }
+//     return result;
+//   }
 
-  // Get the public key
-  getPublicKey() {
-    return this.publicKey;
-  }
+//   // Get the public key
+//   getPublicKey() {
+//     return this.publicKey;
+//   }
 
-  // Get the private key
-  getPrivateKey() {
-    return this.privateKey;
-  }
+//   // Get the private key
+//   getPrivateKey() {
+//     return this.privateKey;
+//   }
+// }
+
+function gcd(a, b) {
+  if (b === BigInt(0)) return a;
+  return gcd(b, a % b);
 }
 
-export default RSA;
+export function extendedEuclid(a, b) {
+  for (let i = 1; i < b; i++) {
+    if ((BigInt(1) + BigInt(i) * b) % a === BigInt(0)) {
+      return (BigInt(1) + BigInt(i) * b) / a;
+    }
+  }
+  return BigInt(0);
+}
+
+export function generateKeys(bits) {
+  const p = BigInt(random(bits));
+  const q = BigInt(random(bits));
+  const n = p * q;
+  const phi = (p - BigInt(1)) * (q - BigInt(1));
+  let e = BigInt(17);
+  do {
+    e += BigInt(1);
+  } while (gcd(e, phi) !== BigInt(1));
+  const d = extendedEuclid(e, phi);
+  return {
+    publicKey: [e, n],
+    privateKey: d,
+  };
+}
+
+export function crypt(data, key, n) {
+  let result = BigInt(1);
+  let base = BigInt(data);
+  let exp = key;
+
+  while (exp > BigInt(0)) {
+    if (exp % BigInt(2) === BigInt(1)) {
+      result = (result * base) % n;
+    }
+    base = (base * base) % n;
+    exp = exp / BigInt(2);
+  }
+
+  return result;
+}
